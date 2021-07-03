bb3c02780930022fd699fb77145edaa8289ebbe0
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-07-03 21:56:24 +0200

    update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,9 @@ import React from "https://dev.jspm.io/react@17.0";
 
 const html = htm.bind(React.createElement);
 
-const REPO_URL = "https://github.com/MichaelCurrin/email-variation";
-const RESOURCES_URL =
-  "https://michaelcurrin.github.io/dev-resources/resources/javascript/packages/react/";
-
 const NAME_KEY = "name";
 
-function TextSample() {
+function Username() {
   const persistedValue = localStorage.getItem(NAME_KEY) ?? "";
   const [value, setValue] = React.useState(persistedValue);
 
@@ -22,12 +18,10 @@ function TextSample() {
     localStorage.setItem(NAME_KEY, value);
   });
 
-  const msg = value
-    ? html`<span>Welcome, <b>${value}</b></span>`
-    : "Tell me your name";
-
   return html`
     <div>
+      <p>Enter the portion of your email that comes before the "@" sign.</p>
+
       <label hmltfor="name-input">Name: </label>
 
       <span> </span>
@@ -36,120 +30,22 @@ function TextSample() {
         id="name-input"
         value=${value}
         onInput=${onInput}
-        placeholder="World"
+        placeholder="foobar123"
       />
-
-      <br />
-      <br />
-
-      <div>${msg}</div>
-
-      <p>
-        <i
-          >This component persists data in <code>localStorage</code>, so it will
-          remember values across a page refresh or restarting the browser.</i
-        >
-      </p>
     </div>
   `;
 }
 
-function ButtonSample() {
-  const [pressed, setPressed] = React.useState(false);
-
-  const onClick = () => {
-    setPressed(!pressed);
-  };
-
-  const msg = pressed ? "on" : "off";
-  const className = pressed ? "button-pressed" : "";
-
+function App() {
   return html`
     <div>
-      <label>Status: </label>
-
-      <br />
-      <br />
-
-      <button
-        className=${className}
-        id="status-button-input"
-        onClick=${onClick}
-      >
-        ${msg}
-      </button>
-    </div>
-  `;
-}
-
-function CheckboxSample() {
-  const [isChecked, setIsChecked] = React.useState(false);
-
-  const onClick = () => {
-    setChecked(!setIsChecked);
-  };
-
-  const msg = isChecked ? "Checked" : "Not checked";
-
-  return html`
-    <div>
-      <label>Teach me React</label>
-      ${" "}
-      <input type="checkbox" id="status-checkbox-input" onClick=${onClick} />
-
-      <p>
-        <i>${msg}</i>
-      </p>
-    </div>
-  `;
-}
-
-function Counter() {
-  const [count, setCount] = React.useState(0);
-
-  return html`
-    <div>
-      <p>You clicked ${count} times</p>
-
-      <button onClick=${() => setCount(count + 1)}>Click me</button>
-    </div>
-  `;
-}
-
-// Unfortunately, spaces needed to be added explicitly, otherwise the Prettier tool strips them out
-// around `a` tags.
-function App(props) {
-  const { name } = props;
-
-  return html`
-    <div>
-      <h2>Greeting with fixed value</h2>
-      <p>Hello, ${name}!</p>
-
-      <h2>Text input</h2>
-      <${TextSample} />
-
-      <h2>Button input</h2>
-      <${ButtonSample} />
-
-      <h2>Toggle input</h2>
-      <${CheckboxSample} />
-
-      <h2>Counter</h2>
-      <${Counter} />
-
-      <br />
-
-      <p>
-        View the ${" "}<a href="${REPO_URL}">source</a>${" "}for this static
-        site on GitHub Pages. Learn more about React in my ${" "}
-        <a href="${RESOURCES_URL}">React resources</a>${" "} guide.
-      </p>
+      <h2>Inputs</h2>
+      <${Username}/>
     </div>
   `;
 }
 
 ReactDOM.render(
-  html`<${App} name="developer" />`,
+  html`<${App}/>`,
   document.getElementById("app")
 );
