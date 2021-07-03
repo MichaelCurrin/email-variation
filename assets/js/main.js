b9a9ceeee2af8cc5d59e8096ddf104ca78475f6d
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-07-03 22:23:13 +0200

    style: Prettier

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,17 +8,16 @@ const NAME_KEY = "name";
 const DOMAIN_KEY = "domain";
 
 function formatEmail(username, suffix, domain) {
-  return `${username}+${suffix}@${domain}`
+  return `${username}+${suffix}@${domain}`;
 }
 
 function datetime(dt) {
-  return `${dt.getFullYear()}-${dt.getMonth()}-${dt.getDate()}-${dt.getHours()}-${dt.getMinutes()}-${dt.getSeconds()}`
+  return `${dt.getFullYear()}-${dt.getMonth()}-${dt.getDate()}-${dt.getHours()}-${dt.getMinutes()}-${dt.getSeconds()}`;
 }
 
 function timestamp(dt) {
-  return dt.getTime()
+  return dt.getTime();
 }
-
 
 function Username() {
   const persistedValue = localStorage.getItem(NAME_KEY) ?? "";
@@ -64,7 +63,10 @@ function Domain() {
 
   return html`
     <div>
-      <p>Enter domain portion of your email. Such as gmail.com or your company's domain.</p>
+      <p>
+        Enter domain portion of your email. Such as gmail.com or your company's
+        domain.
+      </p>
 
       <label hmltfor="domain-input">Domain: </label>
 
@@ -84,43 +86,34 @@ function Result() {
   const username = localStorage.getItem(NAME_KEY) ?? "";
   const domain = localStorage.getItem(DOMAIN_KEY) ?? "";
 
-  const now = new Date()
-  const suffixA = datetime(now)
-  const suffixB = timestamp(now)
+  const now = new Date();
+  const suffixA = datetime(now);
+  const suffixB = timestamp(now);
 
   if (username && domain) {
     return html`
-    <div>
-      <code>
-        ${formatEmail(username, suffixA, domain)}
-      </code>
-      <br/>
-      <code>
-      ${formatEmail(username, suffixB, domain)}
-    </code>
-    </div>
-  `;
-  }
-  else {
-    return html`<i>Nothing to display yet</i>`
+      <div>
+        <code> ${formatEmail(username, suffixA, domain)} </code>
+        <br />
+        <code> ${formatEmail(username, suffixB, domain)} </code>
+      </div>
+    `;
+  } else {
+    return html`<i>Nothing to display yet</i>`;
   }
 }
-
 
 function App() {
   return html`
     <div>
       <h2>Inputs</h2>
-      <${Username}/>
-      <${Domain}/>
+      <${Username} />
+      <${Domain} />
 
       <h2>Outputs</h2>
-      <${Result}/>
+      <${Result} />
     </div>
   `;
 }
 
-ReactDOM.render(
-  html`<${App}/>`,
-  document.getElementById("app")
-);
+ReactDOM.render(html`<${App} />`, document.getElementById("app"));
