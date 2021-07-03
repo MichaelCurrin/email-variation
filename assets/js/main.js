d4f77ec5ec3495c6c73473ba3e9e64ac0fb8ced1
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-07-03 22:01:19 +0200

    feat: add domain

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@ import React from "https://dev.jspm.io/react@17.0";
 const html = htm.bind(React.createElement);
 
 const NAME_KEY = "name";
+const DOMAIN_KEY = "domain";
 
 function Username() {
   const persistedValue = localStorage.getItem(NAME_KEY) ?? "";
@@ -36,11 +37,43 @@ function Username() {
   `;
 }
 
+function Domain() {
+  const persistedValue = localStorage.getItem(DOMAIN_KEY) ?? "";
+  const [value, setValue] = React.useState(persistedValue);
+
+  const onInput = (event) => {
+    setValue(event.target.value);
+  };
+
+  React.useEffect(() => {
+    localStorage.setItem(DOMAIN_KEY, value);
+  });
+
+  return html`
+    <div>
+      <p>Enter domain portion of your email. Such as gmail.com or your company's domain.</p>
+
+      <label hmltfor="domain-input">Domain: </label>
+
+      <span> </span>
+
+      <input
+        id="domain-input"
+        value=${value}
+        onInput=${onInput}
+        placeholder="gmail.com"
+      />
+    </div>
+  `;
+}
+
+
 function App() {
   return html`
     <div>
       <h2>Inputs</h2>
       <${Username}/>
+      <${Domain}/>
     </div>
   `;
 }
